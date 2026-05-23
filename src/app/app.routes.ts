855664cc364a4efa054be84508b2f4fde03ae008
AxelCarrill0 <axelcarrilloe28@gmail.com>
2026-05-23 02:16:38 -0500

    003_Vista Inicial Home_v2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
-import {HomeContentComponent} from './modules/home/components/home-content/home-content.component';
+import { HomeContentComponent } from './modules/home/components/home-content/home-content.component';
 
 
 export const routes: Routes = [
-    {path: '', component: HomeContentComponent}
+    { path: '', component: HomeContentComponent },
+    { path: '**', redirectTo: '' }
 ];
--- src/app/modules/home/components/home-content/home-content.component.ts
+++ src/app/modules/home/components/home-content/home-content.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { CardModule } from 'primeng/card';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-content',
   standalone: true,
-  imports: [ButtonModule, InputTextModule, CardModule],
+  imports: [ButtonModule, InputTextModule, CardModule, RouterModule],
   templateUrl: './home-content.component.html',
-  styleUrl: './home-content.component.scss'
+  styleUrl: './home-content.component.scss',
+
 })
 export class HomeContentComponent {
-
+  visible: boolean = false;
 }
