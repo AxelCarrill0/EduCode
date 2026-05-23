9e94e14328a8dbcb34fad38a1439cab77205f71b
AxelCarrill0 <axelcarrilloe28@gmail.com>
2026-05-23 00:13:57 -0500

    002_Vista Inicial Home

--- src/app/modules/home/components/home-content/home-content.component.ts
+++ src/app/modules/home/components/home-content/home-content.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-home-content',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, InputTextModule, CardModule],
   templateUrl: './home-content.component.html',
   styleUrl: './home-content.component.scss'
 })
