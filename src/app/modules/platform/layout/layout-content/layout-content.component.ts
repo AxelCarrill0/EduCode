51cf00ef6102ab8c71a90b0103edd202ffda90af
AxelCarrill0 <axelcarrilloe28@gmail.com>
2026-05-24 19:39:24 -0500

    011_Correcciones

--- src/app/modules/platform/layout/layout-content/layout-content.component.ts
+++ src/app/modules/platform/layout/layout-content/layout-content.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CardModule } from "primeng/card";
+import { ButtonModule } from "primeng/button";
 
 
 @Component({
   selector: 'app-layout-content',
   standalone: true,
-  imports: [RouterModule, CardModule],
+  imports: [RouterModule, CardModule, ButtonModule],
   templateUrl: './layout-content.component.html',
   styleUrl: './layout-content.component.scss'
 })
